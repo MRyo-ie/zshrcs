88962aba66e79a8a4135d378bdcd5aa6276b7b85
MRyo-ie <e165738@ie.u-ryukyu.ac.jp>
2019-06-10 15:28:48 +0900

    【v1.29】「ターミナルのデザインファイルを追加した。」
    ・ まだ試してないので、テスト次第 v1.30

--- Full/install_zshrc.sh
+++ Full/install_zshrc.sh
@@ -2,6 +2,10 @@
 cp  alias/.zshrc  ~/
 
 # 編集用のシンボリックリンクを作る。
-ln -s  ~/.zshrc.d/.git_repo/Full/.zshenv  ~/.zshrc.d/
-ln -s  ~/.zshrc.d/.git_repo/Full/.zshrc  ~/.zshrc.d/
-ln -s  ~/.zshrc.d/.git_repo/Full/.zshrc.d  ~/.zshrc.d/
+ln -s  ~/.zshrc.d/.git_repository/Full/.zshenv  ~/.zshrc.d/
+ln -s  ~/.zshrc.d/.git_repository/Full/.zshrc  ~/.zshrc.d/
+ln -s  ~/.zshrc.d/.git_repository/Full/.zshrc.d  ~/.zshrc.d/
+
+# ターミナルのデザインファイルを読み込み
+open Pro-Custom.terminal
+
